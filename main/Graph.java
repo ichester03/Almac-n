51febb5bb8dacb4afbe04a37bb015811ce53695f
ichester03 <ahg@usal.es>
2017-12-19 12:37:44 +0100

    fin 1

--- main/Graph.java
+++ main/Graph.java
@@ -1,85 +1,83 @@
 package main;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-	import java.util.ArrayList;
-	import java.util.HashMap;
-	import java.util.List;
-	import java.util.Map;
+/**
+ * Graph object which maps vertex with nodes
+ * 
+ * @author Siddheshwar
+ * 
+ * @param <V>
+ */
+public class Graph<V> {
+	// Vertex i.e. V gets mapped to list of all connecting Nodes.
+	Map<V, List<Node<V>>> adjacencyList;
+	int verticesCount;
+	int edgeCount;
+
+	public Graph() {
+		super();
+		this.adjacencyList = new HashMap<>();
+	}
 
 	/**
-	 * Graph object which maps vertex with nodes
+	 * Add a new node for the given vertex. Vertex to node connection forms
+	 * Edge.
 	 * 
-	 * @author Siddheshwar
-	 * 
-	 * @param <V>
+	 * @param vertex
+	 * @param node
 	 */
-	public class Graph<V> {
-	 // Vertex i.e. V gets mapped to list of all connecting Nodes. 
-	 Map<V, List<Node<V>>> adjacencyList;
-	 int verticesCount;
-	 int edgeCount;
+	public void addNewNode(V vertex, Node<V> node) {
+		List<Node<V>> nodes = adjacencyList.get(vertex);
+		if (nodes == null || nodes.isEmpty()) {
+			nodes = new ArrayList<Node<V>>();
+			nodes.add(node);
+		} else {
+			nodes.add(node);
+		}
+		adjacencyList.put(vertex, nodes);
+	}
 
-	 public Graph() {
-	  super();
-	  this.adjacencyList = new HashMap<>();
-	 }
+	/**
+	 * Takes two vertices and checks if there is a path between v1 and v2.
+	 * Doesn't take vice-versa.
+	 * 
+	 * @param v1
+	 *            first vertex
+	 * @param v2
+	 *            second vertex
+	 * @return
+	 */
+	public boolean hasRelationship(V v1, V v2) {
+		if (v1 == null && v2 == null)
+			return true;
+		if (v1 != null && v2 == null)
+			return true;
+		if (v1 == null && v2 != null)
+			return false;
 
-	 /**
-	  * Add a new node for the given vertex. 
-	         *  Vertex to node connection forms Edge.
-	  * 
-	  * @param vertex
-	  * @param node
-	  */
-	 public void addNewNode(V vertex, Node<V> node) {
-	  List<Node<V>> nodes = adjacencyList.get(vertex);
-	  if (nodes == null || nodes.isEmpty()) { 
-	   nodes = new ArrayList<Node<V>>();
-	   nodes.add(node);
-	  } else {
-	   nodes.add(node);
-	  }
-	  adjacencyList.put(vertex, nodes);
-	 }
+		List<Node<V>> nodes = null;
 
-	 /**
-	  * Takes two vertices and checks if there is a path between v1 and v2.
-	  * Doesn't take vice-versa.
-	  * 
-	  * @param v1
-	  *            first vertex
-	  * @param v2
-	  *            second vertex
-	  * @return
-	  */
-	 public boolean hasRelationship(V v1, V v2) {
-	  if (v1 == null && v2 == null)
-	   return true;
-	  if (v1 != null && v2 == null)
-	   return true;
-	  if (v1 == null && v2 != null)
-	   return false;
+		if (adjacencyList.containsKey(v1)) {
+			nodes = adjacencyList.get(v1);
+			if (nodes != null || !nodes.isEmpty()) {
+				for (Node<V> v : nodes) {
+					if (v.getName().equals(v2))
+						return true;
+				}
+			}
+		}
+		return false;
+	}
 
-	  List<Node<V>> nodes = null;
+	public void print() {
+		System.out.println("Graph is --->");
+		for (V v : adjacencyList.keySet()) {
+			System.out.println(v + " --- " + adjacencyList.get(v));
+		}
+	}
 
-	  if (adjacencyList.containsKey(v1)) {
-	   nodes = adjacencyList.get(v1);
-	   if (nodes != null || !nodes.isEmpty()) {
-	    for (Node<V> v : nodes) {
-	     if (v.getName().equals(v2))
-	      return true;
-	    }
-	   }
-	  }
-	  return false;
-	 }
-
-	 public void print() {
-	  System.out.println("Graph is --->");
-	  for (V v : adjacencyList.keySet()){
-	      System.out.println(v + " --- " + adjacencyList.get(v));
-	                }  
-	 }
-
-	
 }
--- main/Main.java
+++ main/Main.java
@@ -1,10 +1,16 @@
 package main;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 
 public class Main {
 	public static ArrayList<Estanteria> estanterias;
+	static ArrayList<HashMap> pedido;
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
@@ -34,47 +40,43 @@ public class Main {
 		estanterias.add(s6);
 		estanterias.add(s7);
 		estanterias.add(s8);
-		
-		
-		
-		
-		ArrayList<HashMap> pedido = new ArrayList<HashMap>();
-		HashMap <String,Integer> lineaPedido1 = new HashMap<String, Integer>();
-		lineaPedido1.put("patatas",40);
-		lineaPedido1.put("boligrafos",40);
-		lineaPedido1.put("plumas",10);
+
+		pedido = new ArrayList<HashMap>();
+		HashMap<String, Integer> lineaPedido1 = new HashMap<String, Integer>();
+		lineaPedido1.put("patatas", 40);
+		lineaPedido1.put("boligrafos", 40);
+		lineaPedido1.put("plumas", 10);
 		pedido.add(lineaPedido1);
-		
+
 		System.out.println("El pedido contiene: ");
 		System.out.println("==============================");
-		
-		for (HashMap m : pedido){
-			System.out.println("==============================");
-			m.forEach((k,v) -> System.out.println("Pedido ===== Key: " + k + ": Value: " + v));
+
+		for (HashMap m : pedido) {
 			
+			System.out.println("==============================");
+			m.forEach((k, v) -> System.out.println("Pedido ===== Key: " + k + ": Value: " + v));
+
 		}
 		
 		System.out.println("==============================");
-		
-		s1.getObjetos().put("patatas",200);
-		s1.getObjetos().put("melones",100);
-		s2.getObjetos().put("boligrafos",500);
-		s3.getObjetos().put("boligrafos",400);
-		s4.getObjetos().put("melocotones",200);
-		s4.getObjetos().put("berzas",100);
-		s5.getObjetos().put("papeles",500);
-		s6.getObjetos().put("boligrafos",400);
-		
-		
-		s1.getObjetos().put("patatas",400); //lol
-		s1.getObjetos().put("melones",200); //lol
-		
-		s7.getObjetos().put("plumas",500);
-		s8.getObjetos().put("plumas",400);
-		s3.getObjetos().put("colonias",150);
-		s4.getObjetos().put("ratones",210);
-		
-		
+
+		s1.getObjetos().put("patatas", 200);
+		s1.getObjetos().put("melones", 100);
+		s2.getObjetos().put("boligrafos", 500);
+		s3.getObjetos().put("boligrafos", 400);
+		s4.getObjetos().put("melocotones", 200);
+		s4.getObjetos().put("berzas", 100);
+		s5.getObjetos().put("papeles", 500);
+		s6.getObjetos().put("boligrafos", 400);
+
+		s1.getObjetos().put("patatas", 400); // lol
+		s1.getObjetos().put("melones", 200); // lol
+
+		s7.getObjetos().put("plumas", 500);
+		s8.getObjetos().put("plumas", 400);
+		s3.getObjetos().put("colonias", 150);
+		s4.getObjetos().put("ratones", 210);
+
 		Cruce s = new Cruce("s", "ps");
 		Cruce c1 = new Cruce("c1", "pc1");
 		Cruce c2 = new Cruce("c2", "pc2");
@@ -82,8 +84,8 @@ public class Main {
 		Cruce c4 = new Cruce("c4", "pc4");
 		Cruce t = new Cruce("t", "pt");
 
-		
-		//grafo de http://geekrai.blogspot.com.es/2014/07/graph-implementation-in-java.html
+		// grafo de
+		// http://geekrai.blogspot.com.es/2014/07/graph-implementation-in-java.html
 		Graph<String> graph = new Graph<String>();
 		graph.addNewNode("ps1", new Node<String>("pc1", 5));
 		graph.addNewNode("pc1", new Node<String>("ps1", 5));
@@ -93,8 +95,7 @@ public class Main {
 		graph.addNewNode("pc3", new Node<String>("ps3", 5));
 		graph.addNewNode("ps4", new Node<String>("pc4", 5));
 		graph.addNewNode("pc4", new Node<String>("ps4", 5));
-		
-		
+
 		graph.addNewNode("ps5", new Node<String>("pc1", 5));
 		graph.addNewNode("pc1", new Node<String>("ps5", 5));
 		graph.addNewNode("ps6", new Node<String>("pc2", 5));
@@ -103,66 +104,180 @@ public class Main {
 		graph.addNewNode("pc3", new Node<String>("ps7", 5));
 		graph.addNewNode("ps8", new Node<String>("pc4", 5));
 		graph.addNewNode("pc4", new Node<String>("ps8", 5));
-		
-		
-		
-		
+
 		graph.addNewNode("ps", new Node<String>("pc1", 10));
-	    graph.addNewNode("pc1", new Node<String>("ps", 10));
-	    graph.addNewNode("pc1", new Node<String>("pc2", 10));
-	    graph.addNewNode("pc2", new Node<String>("pc1", 10));
-	    graph.addNewNode("pc2", new Node<String>("pc3", 10));
-	    graph.addNewNode("pc3", new Node<String>("pc2", 10));
-	    graph.addNewNode("pc3", new Node<String>("pc4", 10));
-	    graph.addNewNode("pc4", new Node<String>("pc3", 10));
-	    graph.addNewNode("pc4", new Node<String>("pt", 10));
-	    graph.addNewNode("pt", new Node<String>("pc4", 10));
-	    
+		graph.addNewNode("pc1", new Node<String>("ps", 10));
+		graph.addNewNode("pc1", new Node<String>("pc2", 10));
+		graph.addNewNode("pc2", new Node<String>("pc1", 10));
+		graph.addNewNode("pc2", new Node<String>("pc3", 10));
+		graph.addNewNode("pc3", new Node<String>("pc2", 10));
+		graph.addNewNode("pc3", new Node<String>("pc4", 10));
+		graph.addNewNode("pc4", new Node<String>("pc3", 10));
+		graph.addNewNode("pc4", new Node<String>("pt", 10));
+		graph.addNewNode("pt", new Node<String>("pc4", 10));
+
 		graph.print();
 		System.out.println("==============================");
 		System.out.println(" Path PS and PC1 exists ? :" + graph.hasRelationship("ps", "pc1"));
 		System.out.println(" Path PC1 and PS exists ? :" + graph.hasRelationship("pc1", "ps"));
 		System.out.println("==============================");
-		
-		s1.getObjetos().forEach((k,v) -> System.out.println("Estantería |S1| ===== Key: " + k + ": Value: " + v));
-		s2.getObjetos().forEach((k,v) -> System.out.println("Estantería |S2| ===== Key: " + k + ": Value: " + v));
-		s3.getObjetos().forEach((k,v) -> System.out.println("Estantería |S3| ===== Key: " + k + ": Value: " + v));
-		s4.getObjetos().forEach((k,v) -> System.out.println("Estantería |S4| ===== Key: " + k + ": Value: " + v));
-		s5.getObjetos().forEach((k,v) -> System.out.println("Estantería |S5| ===== Key: " + k + ": Value: " + v));
-		s6.getObjetos().forEach((k,v) -> System.out.println("Estantería |S6| ===== Key: " + k + ": Value: " + v));
-		s7.getObjetos().forEach((k,v) -> System.out.println("Estantería |S7| ===== Key: " + k + ": Value: " + v));
-		s8.getObjetos().forEach((k,v) -> System.out.println("Estantería |S8| ===== Key: " + k + ": Value: " + v));
+
+		s1.getObjetos().forEach((k, v) -> System.out.println("Estantería |S1| ===== Key: " + k + ": Value: " + v));
+		s2.getObjetos().forEach((k, v) -> System.out.println("Estantería |S2| ===== Key: " + k + ": Value: " + v));
+		s3.getObjetos().forEach((k, v) -> System.out.println("Estantería |S3| ===== Key: " + k + ": Value: " + v));
+		s4.getObjetos().forEach((k, v) -> System.out.println("Estantería |S4| ===== Key: " + k + ": Value: " + v));
+		s5.getObjetos().forEach((k, v) -> System.out.println("Estantería |S5| ===== Key: " + k + ": Value: " + v));
+		s6.getObjetos().forEach((k, v) -> System.out.println("Estantería |S6| ===== Key: " + k + ": Value: " + v));
+		s7.getObjetos().forEach((k, v) -> System.out.println("Estantería |S7| ===== Key: " + k + ": Value: " + v));
+		s8.getObjetos().forEach((k, v) -> System.out.println("Estantería |S8| ===== Key: " + k + ": Value: " + v));
 		System.out.println("==============================");
+
+		restarUds("ps1", "patatas", 100);
+
+		s1.getObjetos().forEach((k, v) -> System.out.println("Estantería |S1| ===== Key: " + k + ": Value: " + v));
+
+		aEstrella(graph, "pc1");
 		
 		
-		restarUds("ps1","patatas",100);
 		
-		s1.getObjetos().forEach((k,v) -> System.out.println("Estantería |S1| ===== Key: " + k + ": Value: " + v));
+		ArrayList<ElementoAbierto> arraylist = new ArrayList<ElementoAbierto>();
+		arraylist.add(new ElementoAbierto("ps1",30));
+		arraylist.add(new ElementoAbierto("ps5",45));
+		arraylist.add(new ElementoAbierto("pc2",40));
+		
+		
+		for(ElementoAbierto str: arraylist){
+			System.out.println(str);
+	   }
+
+	   /* Sorting on Rollno property*/
+	   System.out.println("RollNum Sorting:");
+	   Collections.sort(arraylist, ElementoAbierto.ElementComparator);
+	   for(ElementoAbierto str: arraylist){
+			System.out.println(str);
+	   }
 	}
-	
-	public static void restarUds(String idEstanteria,String objeto, int unidades){
-		
-		for (Estanteria e : estanterias){
-			if(e.pos.equals(idEstanteria)){
-				if(e.getObjetos().containsKey(objeto)){
+
+	public static void restarUds(String idEstanteria, String objeto, int unidades) {
+
+		for (Estanteria e : estanterias) {
+			if (e.pos.equals(idEstanteria)) {
+				if (e.getObjetos().containsKey(objeto)) {
 					System.out.println("Existe objeto");
-					
+
 					int actual = e.getObjetos().get(objeto);
 					int act = actual - unidades;
-					if (act>=0){
+					if (act >= 0) {
 						e.getObjetos().put(objeto, act);
-					}else{
-						System.out.println("No hay suficientes " + objeto + " solo hay " + actual + " tu pedido es de " + unidades);
+					} else {
+						System.out.println("No hay suficientes " + objeto + " solo hay " + actual + " tu pedido es de "
+								+ unidades);
 					}
-					
-					
-				}else{
+
+				} else {
 					System.out.println("No existe objeto: " + objeto);
 				}
 			}
-			
+
 		}
+
+	}
+
+	public static <V> void aEstrella(Graph<String> gr, String pos) {
+
+		ArrayList<String> cerrados = new ArrayList();
+
+		cerrados.add(pos);
+
+		int g = 0;
+		int h = 0;
+		int f = 0;
+
+		System.out.println("lista de ps" + gr.adjacencyList.get(pos));
+		List<Node<String>> conexiones = gr.adjacencyList.get(pos);
+
+		for (int i = 0; i < conexiones.size(); i++) {
+			System.out.println(pos + "está conectado con " + conexiones.get(i).getName() + " Con peso "
+					+ conexiones.get(i).getWeight());
+
+			if (cerrados.contains(conexiones.get(i).getName())) {
+				System.out.println("Contiene");
+			} else {
+				System.out.println("NOOOOOO Contiene");
+
+				g = g + conexiones.get(i).getWeight();
+				System.out.println("la g es " + g);
+				System.out.println(conexiones.get(i));
+				if (esPasillo(conexiones.get(i).name)) {
+
+				h = 10 * pedido.get(0).size();
+				System.out.println("la h es " + h);
+				f= g+h;
+				}else{ //estanteria
+				
+				int num_coincidencias = compruebaPedidoEstateria(conexiones.get(i).name);
+				h = (pedido.get(0).size() - num_coincidencias) * 10;
+				System.out.println("la h es " + h);
+				f = g + h;
+				}
+				
+				
+				
 			
+			}
+			//
+			// for (String key : gr.adjacencyList.keySet()) {
+			// System.out.println(key);
+			// lol(gr, key);
+			// System.out.println(gr.adjacencyList.get(key));
+			// }
+		}
+	}
+
+	private static boolean esPasillo(String name) {
+
+		for (Estanteria e : estanterias) {
+			if (e.getPos().equals(name)) {
+				return false;
+			}
+		}
+		return true;
+
+	}
+	
+	private static int compruebaPedidoEstateria(String nameEstanteria){
+		
+		int cant = 0;
+		
+		for (Estanteria e : estanterias) {
+			if (e.getPos().equals(nameEstanteria)) {
+				
+
+				
+				
+				 for (String key : e.getObjetos().keySet()) {
+				 
+					 if (pedido.get(0).get(key) != null){
+						cant++; 
+					 }
+					 
+				 }
+				
+				
+			}
+		}
+		
+		
+		
+		
+		return cant;	
+	}
+
+	static void lol(Graph<String> g, String k) {
+		List<Node<String>> nodes = g.adjacencyList.get(k);
+		for (int i = 0; i < nodes.size(); i++) {
+			System.out.println(nodes.get(i).getWeight());
+		}
 	}
 
 }
